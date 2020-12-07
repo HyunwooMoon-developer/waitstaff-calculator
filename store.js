90b22b6ee0f1dfd11fd9a061b6b28014062cb781
Hyunwoo Moon <mhw9163@gmail.com>
2020-12-06 16:52:10 -0800

    finished

---
+++ store.js
@@ -0,0 +1,20 @@
+const subtotal = 0;
+const tipPrice = 0;
+const total = 0;
+
+const earning  = {
+    tipTotal : 0,
+    mealCount : 0,
+    average : 0
+}
+
+
+
+
+export default {
+
+    subtotal,
+    tipPrice,
+    total,
+    earning
+}
